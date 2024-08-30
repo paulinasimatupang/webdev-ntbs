90424c97e63b3e54a30f687cdd538c2ceaea9686
paulina <paulinalestarisimatupang@gmail.com>
2024-08-30 09:52:20 +0700

    detail-request

--- resources/views/apps/calon_nasabah/detail-request.blade.php
+++ resources/views/apps/calon_nasabah/detail-request.blade.php
@@ -20,268 +20,86 @@
     <div class="col-md-12">
         <div class="card mb-5">
             <div class="card-body">
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">NIK</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">{{ $nasabah->no_identitas }}</p>
-                        </div>
+                @php
+                $fields = [
+                'NIK' => $nasabah->no_identitas,
+                'Nama Lengkap' => $nasabah->nama_lengkap,
+                'Nama Alias' => $nasabah->nama_alias,
+                'Ibu Kandung' => $nasabah->ibu_kandung,
+                'Tempat Lahir' => $nasabah->tempat_lahir,
+                'Tanggal Lahir' => \Carbon\Carbon::parse($nasabah->tgl_lahir)->format('d M Y'),
+                'Jenis Kelamin' => $jenis_kelamin->firstWhere('seq', $nasabah->jenis_kelamin)->opt_label ?? '',
+                'Agama' => $agama->firstWhere('seq', $nasabah->agama)->opt_label ?? '',
+                'Alamat' => $nasabah->alamat,
+                'RT' => $nasabah->rt,
+                'RW' => $nasabah->rw,
+                'Kecamatan' => $nasabah->kecamatan,
+                'Kelurahan' => $nasabah->kelurahan,
+                'Kabupaten/Kota' => $kab_kota->firstWhere('seq', $nasabah->kab_kota)->opt_label ?? $nasabah->kab_kota,
+                'Provinsi' => $provinsi->firstWhere('seq', $nasabah->provinsi)->opt_label ?? '',
+                'Kode POS' => $nasabah->kode_pos,
+                'Nomor Telepon' => $nasabah->no_telp,
+                'Nomor HP' => $nasabah->no_hp,
+                'Status Penduduk' => $status_penduduk->firstWhere('seq', $nasabah->status_penduduk)->opt_label ?? '',
+                'Kewarganegaraan' => $kewarganegaraan->firstWhere('seq', $nasabah->kewarganegaraan)->opt_label ?? '',
+                'Nomor NPWP' => $nasabah->npwp,
+                'Jenis Identitas' => $jenis_identitas->firstWhere('seq', $nasabah->jenis_identitas)->opt_label ?? '',
+                'Golongan Darah' => $golongan_darah->firstWhere('seq', $nasabah->golongan_darah)->opt_label ?? '',
+                'Expired Identitas' => \Carbon\Carbon::parse($nasabah->expired_identitas)->format('d M Y'),
+                'Pendidikan Terakhir' => $pendidikan_terakhir->firstWhere('seq', $nasabah->pendidikan_terakhir)->opt_label ?? '',
+                'Email' => $nasabah->email,
+                ];
+                @endphp
+
+                @foreach ($fields as $label => $value)
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">{{ $label }}</label>
+                    <div class="col-sm-10">
+                        <p class="form-control-static">{{ $value }}</p>
                     </div>
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Nama Lengkap</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">{{ $nasabah->nama_lengkap }}</p>
-                        </div>
+                </div>
+                @endforeach
+
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Foto Diri</label>
+                    <div class="col-sm-10">
+                        @if ($nasabah->foto_diri)
+                            <img src="{{ $baseUrl . $nasabah->foto_diri }}" alt="Foto Tanda Tangan" class="img-thumbnail" style="max-width: 200px;">
+                        @else
+                        <p class="form-control-static">Belum diunggah</p>
+                        @endif
                     </div>
+                </div>
 
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Nama Alias</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">{{ $nasabah->nama_alias }}</p>
-                        </div>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Foto KTP</label>
+                    <div class="col-sm-10">
+                        @if ($nasabah->foto_ktp)
+                        <img src="{{ $baseUrl . $nasabah->foto_ktp }}" alt="Foto Tanda Tangan" class="img-thumbnail" style="max-width: 200px;">
+                        @else
+                        <p class="form-control-static">Belum diunggah</p>
+                        @endif
                     </div>
+                </div>
 
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Ibu Kandung</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">{{ $nasabah->ibu_kandung }}</p>
-                        </div>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label">Foto Tanda Tangan</label>
+                    <div class="col-sm-10">
+                        @if ($nasabah->foto_tanda_tangan)
+                        <img src="{{ $baseUrl . $nasabah->foto_tanda_tangan }}" alt="Foto Tanda Tangan" class="img-thumbnail" style="max-width: 200px;">
+                        @else
+                        <p class="form-control-static">Belum diunggah</p>
+                        @endif
                     </div>
+                </div>
 
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Tempat Lahir</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">{{ $nasabah->tempat_lahir }}</p>
-                        </div>
+                <div class="form-group row">
+                    <div class="col-sm-12 text-right">
+                        <a href="{{ route('nasabah_request') }}">
+                            <button type="button" class="btn btn-primary">Back</button>
+                        </a>
                     </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Tanggal Lahir</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">{{ $nasabah->tgl_lahir }}</p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Jenis Kelamin</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">
-                                @foreach($jenis_kelamin as $option)
-                                    @if($nasabah->jenis_kelamin == $option->seq)
-                                        {{ $option->opt_label }}
-                                    @endif
-                                @endforeach
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Agama</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">
-                                @foreach($agama as $option)
-                                    @if($nasabah->agama == $option->seq)
-                                        {{ $option->opt_label }}
-                                    @endif
-                                @endforeach
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Alamat</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">{{ $nasabah->alamat }}</p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">RT</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">{{ $nasabah->rt }}</p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">RW</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">{{ $nasabah->rw }}</p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Kecamatan</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">{{ $nasabah->kecamatan }}</p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Kelurahan</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">{{ $nasabah->kelurahan }}</p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Kabupaten/Kota</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">
-                                @php
-                                    $labelFound = false;
-                                @endphp
-                                
-                                @foreach($kab_kota as $option)
-                                    @if($nasabah->kab_kota == $option->seq)
-                                        {{ $option->opt_label }}
-                                        @php
-                                            $labelFound = true;
-                                        @endphp
-                                    @endif
-                                @endforeach
-
-                                @if(!$labelFound)
-                                    {{ $nasabah->kab_kota }}
-                                @endif
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Provinsi</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">
-                                @foreach($provinsi as $option)
-                                    @if($nasabah->provinsi == $option->seq)
-                                        {{ $option->opt_label }}
-                                    @endif
-                                @endforeach
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Kode POS</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">{{ $nasabah->kode_pos }}</p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Nomor Telepon</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">{{ $nasabah->no_telp }}</p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Nomor HP</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">{{ $nasabah->no_hp }}</p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Status Penduduk</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">
-                                @foreach($status_penduduk as $option)
-                                    @if($nasabah->status_penduduk == $option->seq)
-                                        {{ $option->opt_label }}
-                                    @endif
-                                @endforeach
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Kewarganegaraan</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">
-                                @foreach($kewarganegaraan as $option)
-                                    @if($nasabah->kewarganegaraan == $option->seq)
-                                        {{ $option->opt_label }}
-                                    @endif
-                                @endforeach
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Nomor NPWP</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">{{ $nasabah->npwp }}</p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Jenis Identitas</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">
-                                @foreach($jenis_identitas as $option)
-                                    @if($nasabah->jenis_identitas == $option->seq)
-                                        {{ $option->opt_label }}
-                                    @endif
-                                @endforeach
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Golongan Darah</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">
-                                @foreach($golongan_darah as $option)
-                                    @if($nasabah->golongan_darah == $option->seq)
-                                        {{ $option->opt_label }}
-                                    @endif
-                                @endforeach
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Expired Identitas</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">{{ $nasabah->expired_identitas }}</p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Pendidikan Terakhir</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">
-                                @foreach($pendidikan_terakhir as $option)
-                                    @if($nasabah->pendidikan_terakhir == $option->seq)
-                                        {{ $option->opt_label }}
-                                    @endif
-                                @endforeach
-                            </p>
-                        </div>
-                    </div>
-
-                    <div class="form-group row">
-                        <label class="col-sm-2 col-form-label">Email</label>
-                        <div class="col-sm-10">
-                            <p class="form-control-static">{{ $nasabah->email }}</p>
-                        </div>
-                    </div>
-
-                    <div class="col-sm-12 d-flex justify-content-end">
-                        <a href="{{ route('nasabah_request') }}" class="btn btn-primary mr-2">Back</a>
-                        <form id="actionForm" method="POST" class="d-inline">
-                            @csrf
-                            <input type="hidden" name="nasabah_id" value="{{ $nasabah->id }}">
-                            <input type="hidden" name="action" id="formAction">
-
-                            <button type="button" id="activateMerchantBtn" class="btn btn-success">
-                                Approve
-                            </button>
-
-                            <button type="button" id="rejectMerchantBtn" class="btn btn-danger ml-2">
-                                Reject
-                            </button>
-                        </form>
-                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -289,23 +107,6 @@
 @endsection
 
 @section('page-js')
-<script>
-document.getElementById('activateMerchantBtn').addEventListener('click', function() {
-    if (confirm('Are you sure you want to activate this merchant?')) {
-        document.getElementById('formAction').value = 'activate';
-        document.getElementById('actionForm').action = "{{ route('nasabah_accept', ['id' => $nasabah->id]) }}";
-        document.getElementById('actionForm').submit();
-    }
-});
-
-document.getElementById('rejectMerchantBtn').addEventListener('click', function() {
-    if (confirm('Are you sure you want to reject this merchant?')) {
-        document.getElementById('formAction').value = 'reject';
-        document.getElementById('actionForm').action = "{{ route('nasabah_reject', ['id' => $nasabah->id]) }}";
-        document.getElementById('actionForm').submit();
-    }
-});
-</script>
 @endsection
 
 @section('bottom-js')
