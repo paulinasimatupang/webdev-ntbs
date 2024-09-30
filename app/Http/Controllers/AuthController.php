e27d0e62978a4ebbc6aef424dc879429862182e9
fathiaqurrata <123913816+fathiaqurrata@users.noreply.github.com>
2024-09-30 11:38:08 +0700

    ..

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -147,13 +147,21 @@ class AuthController extends Controller
             return response()->json(['status' => false, 'message' => 'All fields are required'], 400);
         }
 
+        $existingFingerprint = User::where('finger_print', $fingerPrint)
+                                    ->where('id', '!=', $id) 
+                                    ->first();
+
+        if ($existingFingerprint) {
+            Log::warning("Fingerprint already registered by another user.");
+            return response()->json(['status' => false, 'message' => 'Fingerprint already registered by another user'], 409);
+        }
+
         $existingUser = User::where('id', $id)
                             ->where('finger_print', $fingerPrint)
                             ->first();
 
         if ($existingUser) {
             Log::warning("Fingerprint already registered for user ID: {$id}");
-            Log::warning("Fingerprint already registered for user ID: {$fingerPrint}");
             return response()->json(['status' => false, 'message' => 'Fingerprint already registered for this user'], 409);
         }
 
@@ -172,6 +180,7 @@ class AuthController extends Controller
             return response()->json(['status' => false, 'message' => 'Failed to register fingerprint'], 500);
         }
     }
+
 
 
 
